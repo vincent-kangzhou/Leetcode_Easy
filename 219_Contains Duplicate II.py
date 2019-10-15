ac518562c437eccd5de741fed8019f3c69347a02
kang_vincent_zhou <kangzhou.vicent@gmail.com>
2019-10-15 00:58:16 -0400

    Update 219_Contains Duplicate II.py

--- 219_Contains Duplicate II.py
+++ 219_Contains Duplicate II.py
@@ -2,7 +2,7 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         mapping={}
         for i in range(len(nums)):
-            if nums[i] in mapping and abs(mapping[nums[i]]-i)<=k:
+            if nums[i] in mapping and i-mapping[nums[i]]<=k:
                 return True
             else:
                 
