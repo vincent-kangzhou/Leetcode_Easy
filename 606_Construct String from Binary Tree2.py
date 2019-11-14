24c99f2d710410eb457b1a53611221aea67995c4
kang_vincent_zhou <kangzhou.vicent@gmail.com>
2019-11-14 18:03:42 -0500

    Create 606_Construct String from Binary Tree2.py

---
+++ 606_Construct String from Binary Tree2.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        if not t: return ''
+        
+        res=str(t.val)
+        if  t.left or  t.right:
+            res+='('+self.tree2str(t.left)+')'
+        if t.right:
+            res+='('+self.tree2str(t.right)+')'
+
+        return res
